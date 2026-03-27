5c4c803d2d6e6e5d0aee7a586a63f1422c1c5b22
Nasif Choudhury <nasif.c7@gmail.com>
2026-03-27 19:20:32 -0400

    fix: use getSentryExpoConfig for Expo-compatible metro config

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,4 @@
 // metro.config.js
-const { getDefaultConfig } = require("expo/metro-config");
-const { withSentryConfig } = require("@sentry/react-native/metro");
+const { getSentryExpoConfig } = require("@sentry/react-native/metro");
 
-const config = getDefaultConfig(__dirname);
-
-module.exports = withSentryConfig(config);
+module.exports = getSentryExpoConfig(__dirname);
